376e5b3d92a49f94f187e0b906ba5b78c1b14d05
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-03-24 18:59:44 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,27 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		char[] arr = {'b', 'f', 'a', 'c', 'g', 'd', 'e'};
+		insertionSort(arr);
+		for (int i = 0; i<arr.length;i++){
+			System.out.println(arr[i] + " ");
+		}
 	}
 
+public static void insertionSort(char[] characters) {
+char key;
+int index;
+for (int i = 1; i<characters.length;i++){
+key = characters[i];
+index = i-1;
+while (index>=0&& characters[index]>key){
+characters[index+1] = characters[index];
+index--;
+}
 
+characters[index+1] = key;
+}
+
+
+}
 }
